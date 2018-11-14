90cb7d129fab417145c86164cfa7a6a28619b234
Valerii Sosov <sosov.valeriy@gmail.com>
2018-11-15 01:29:08 +0200

    Initial commit

---
+++ app/src/main/java/com/guralnya/weatherforever/BaseApplication.java
@@ -0,0 +1,20 @@
+package com.guralnya.weatherforever;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder().name("weather.realm").build();
+        Realm.setDefaultConfiguration(config);
+
+        //Realm.deleteRealm(config);
+    }
+}
