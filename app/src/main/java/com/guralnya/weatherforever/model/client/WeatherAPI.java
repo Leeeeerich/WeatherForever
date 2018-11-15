718608c41ff691de915a7ef66a4d8356c6cdb226
Valerii Sosov <sosov.valeriy@gmail.com>
2018-11-16 00:42:59 +0200

    Add weather server objects and weather API Interface

---
+++ app/src/main/java/com/guralnya/weatherforever/model/client/WeatherAPI.java
@@ -0,0 +1,44 @@
+package com.guralnya.weatherforever.model.client;
+
+import com.guralnya.weatherforever.model.objects.WeatherDay;
+import com.guralnya.weatherforever.model.objects.WeatherForecast;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public class WeatherAPI {
+    public static String KEY = "f90f6d2ada0ffbb1f2ba3fe660040e1f";
+    public static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    private static Retrofit retrofit = null;
+
+    public interface ApiInterface {
+        @GET("weather")
+        Call<WeatherDay> getToday(
+                @Query("lat") Double lat,
+                @Query("lon") Double lon,
+                @Query("units") String units,
+                @Query("appid") String appid
+        );
+
+        @GET("forecast")
+        Call<WeatherForecast> getForecast(
+                @Query("lat") Double lat,
+                @Query("lon") Double lon,
+                @Query("units") String units,
+                @Query("appid") String appid
+        );
+    }
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
